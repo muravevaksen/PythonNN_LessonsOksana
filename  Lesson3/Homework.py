aa42a4c5d2859fc6ee7a840b5b28865cca8e4852
murav <muravevaksen@gmail.com>
2023-10-31 19:35:17 +0400

    add lesson 4

---
+++  Lesson3/Homework.py
@@ -0,0 +1,11 @@
+#1
+var1 = 'this is text'
+var2 = var1
+var1 = 'there is text'
+print('var1 = ', var1, '\nvar2 = ', var2)
+
+#2
+var3 = ['this', 'is', 'text']
+var4 = var3
+var3[0] = 'there'
+print('var3 = ', var3, '\nvar4 = ', var4)
