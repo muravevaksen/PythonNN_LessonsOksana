b2f8f6fe97a7adce544eae81d12c0c5bad92a0ad
murav <muravevaksen@gmail.com>
2023-11-10 09:04:09 +0400

    lesson10

---
+++ Lesson10/methods.py
@@ -0,0 +1,16 @@
+#replace
+
+var1 = 'hello world'
+var2 = var1.replace('l', '0')
+print(var2)
+
+# split - разделяет на строки по разделителю; rsplit - справа налево
+
+var3 = var1.split('l')
+print(var3)
+
+#partititon - создает список из строки по разделителю
+
+var4 = var1.partition('l')
+print(var4)
+
---
+++ Lesson9/Homework.py
@@ -0,0 +1,9 @@
+def number_to_string(num):
+    return str(num)
+
+print(number_to_string(-100))
+
+def remove_char(s):
+    return s[1:len(s)-1]
+
+print(remove_char('ttrandko'))
