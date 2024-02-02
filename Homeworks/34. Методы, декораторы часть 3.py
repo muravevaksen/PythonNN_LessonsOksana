9fbf3c5257d060f66cec81ab89388454b47d0c79
murav <muravevaksen@gmail.com>
2024-02-03 02:19:18 +0400

    + last hws 2024

--- Homeworks/34. Методы, декораторы часть 3.py
+++ Homeworks/34. Методы, декораторы часть 3.py
@@ -16,5 +16,30 @@ def God():
 
 print(God())
 
+print('---'*100)
+
 # 2 OOP: Object Oriented Piracy
 
+class Ship:
+    def __init__(self, draft, crew):
+        self.draft = draft
+        self.crew = crew
+
+    def is_worth_it(self):
+        if self.draft - (1.5 * self.crew) > 20:
+            return True
+        else:
+            return False
+
+Titanic = Ship(0, 0)
+Titanic.is_worth_it()
+
+print('---'*100)
+
+# 3 Classic Hello World
+
+class Solution():
+    def main(*args):
+        print("Hello World!")
+
+Solution.main("parameter1", "parameter2", "parametern")
